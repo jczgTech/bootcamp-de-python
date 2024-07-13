ab116a91ef1c3f59abc454f4a903372614cc11e6
Camilo Zuniga <jczg.tech@gmail.com>
2024-07-13 15:52:42 -0500

    se inicializa el repositorio de python

---
+++ tipos-de-datos.py
@@ -0,0 +1,14 @@
+"string" # para tipos de datos de texto
+'string'
+"""string"""
+'''string'''
+"""Tus datos son 
+    nombre: Camilo
+    apellido: Zuñiga"""
+#tipos de datos numéricos
+4#tipo de dato entero
+4.5#tipo de dato float
+
+#tipos de datos booleanos 
+True#True va con T mayúscula
+False#False va con F mayúscula
