5109bbd6a3664f32440f48fe190dba020cc4ea83
Camilo Zuniga <jczg.tech@gmail.com>
2024-07-13 21:31:17 -0500

    Avanzamos variables

---
+++ variables/variables.py
@@ -0,0 +1,13 @@
+nombre = "Camilo"
+bienvenida = f"Hola {nombre} feliz noche"
+
+print(bienvenida)
+print("ola" in bienvenida)
+print("Betty" not in bienvenida)
+print("Hola" not in bienvenida)
+
+#CamelCase
+nombreCompletoDeTuProfe = "Jheyson Galvis"
+
+#SnakeCase
+nombre_completo_de_tu_profe = "Jheyson Galvis"
