b839328949176ca2d6671142218dbb72416dd368
Camilo Zuniga <jczg.tech@gmail.com>
2024-07-15 19:47:32 -0500

    Se abordaron los fundamentos de Python

---
+++ fundamentos.py
@@ -0,0 +1,13 @@
+name = "Camilo"
+print(type(name))
+print(name[0:6])
+last_name = "Zuñiga Giron "
+nombre = name + last_name
+#print(nombre[0:19] * 5)
+print(len(nombre))
+
+print(nombre.lower())
+print(nombre.upper())
+print(nombre.strip())
+
+print(name + " " + last_name)
