2ea88a26c3056b373feb408cf623d853dd8b70e9
Camilo Zuniga <jczg.tech@gmail.com>
2024-07-22 18:49:45 -0500

    Abordamos operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,22 @@
+#sumas y restas ( + y -)
+suma = 2024 + 2000
+print(suma)
+resta = 2024 - 2000
+print(resta)
+#multiplicacion y division( * y /)
+multiplicacion = 23 * 7
+print(multiplicacion)
+division = 8 / 2
+print(division)
+#potenciacion
+potencia = 2 ** 3
+print(potencia)
+#division entera (//)
+division_entera = 12 // 4
+print(division_entera)
+#resto (%)
+resto = 12 % 4
+print(resto)
+print(suma, resta, multiplicacion)
+tipo_de_dato = type(division)
+print(tipo_de_dato)
